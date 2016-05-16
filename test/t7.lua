b95f83914285882491aa5410578a36fbbf2ba670
Patrick Rigney <patrick@toggledbits.com>
2016-05-16 10:01:19 -0400

    New test

---
+++ test/t7.lua
@@ -0,0 +1,6 @@
+test = require('test/test')
+testeval = test.testeval -- bring into global space
+
+print("### Luaxp functional test -- user variables and functions")
+
+-- need to add a way to create and pass context to testeval()
