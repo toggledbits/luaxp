67420ce9340c835adddff54318859b64ef126f7d
Patrick Rigney <patrick@toggledbits.com>
2016-05-16 20:48:40 -0400

    Use error handling in test result evaluation

--- test/test.lua
+++ test/test.lua
@@ -3,14 +3,21 @@ luaxp = require('luaxp')
 _T = {}
 
 local n = 0
+local fail = "*** FAIL ***"
 
 _T.testeval = function(test, expect)
 	n = n + 1
-	local r = luaxp.evaluate(test)
-	local s
-	if (string.find(expect, "%%") ~= nil) then
-		if (string.find(tostring(r), expect) ~= nil) then s = "MATCH" else s = "*** FAIL ***" end
-	elseif (tostring(r) == expect) then s = "OK" else s = "*** FAIL ***" end
+	local r,m = luaxp.evaluate(test)
+	local s = fail
+	if (r == nil) then 
+		r = "error(" .. tostring(m) .. ")" 
+	elseif (string.find(expect, "%%") ~= nil) then
+		if (string.find(tostring(r), expect) ~= nil) then 
+			s = "MATCH" 
+		end
+	elseif (tostring(r) == expect) then 
+		s = "OK" 
+	end
 	print(n, test, expect, tostring(r), s)
 end
 
