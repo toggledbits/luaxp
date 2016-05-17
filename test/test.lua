0b6428be7c76f1b30e8fb70ecd8a84a4e587f2b6
Patrick Rigney <patrick@toggledbits.com>
2016-05-17 08:55:15 -0400

    Support for context in test

--- test/test.lua
+++ test/test.lua
@@ -5,20 +5,25 @@ _T = {}
 local n = 0
 local fail = "*** FAIL ***"
 
-_T.testeval = function(test, expect)
+_T.testeval = function(test, expect, ctx)
+	local status = false
+	ctx = ctx or {}
 	n = n + 1
-	local r,m = luaxp.evaluate(test)
+	local r,m = luaxp.evaluate(test, ctx)
 	local s = fail
 	if (r == nil) then 
 		r = "error(" .. tostring(m) .. ")" 
 	elseif (string.find(expect, "%%") ~= nil) then
 		if (string.find(tostring(r), expect) ~= nil) then 
 			s = "MATCH" 
+			status = true
 		end
 	elseif (tostring(r) == expect) then 
 		s = "OK" 
+		status = true
 	end
 	print(n, test, expect, tostring(r), s)
+	return status
 end
 
 return _T
