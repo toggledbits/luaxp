61a321aad3fad66955d6cd185f273a0d4b05e3af
Patrick Rigney <patrick@toggledbits.com>
2018-02-02 14:50:31 -0500

    Remove files not needed for future tests (replaced).

--- test/t2.lua
+++
@@ -1,9 +0,0 @@
-test = require('test/test')
-testeval = test.testeval -- bring into global space
-
-print("### Luaxp functional test -- string parsing")
-
-testeval("\"The rain in Spain\"", "The rain in Spain")
-testeval("'The rain in Spain'", "The rain in Spain")
-testeval("'\\\\'", "\\")
-testeval('"inside \\"quotes\\""', "inside \"quotes\"")
